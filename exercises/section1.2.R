0282df0efb6b5a2c9b9adcb97f040dbdc11e7412
Johnson <stoiqed@gmail.com>
2025-05-25 23:47:46 +0800

    Solve up to problems 1.2.9

--- exercises/section1.2.R
+++ exercises/section1.2.R
@@ -1,5 +1,43 @@
-# 1.2.1
+# ------------------- 1.2.1
 # 
 # S = { RRR, RRB, RBR, RBB, BRR, BRB, BBR, BBB }
 
-# 1.2.3
+# ------------------- 1.2.3
+#
+# Let the 3 books be A, X, Y, where X and Y are the 2 volumes
+#
+# S = { AXY, XAY, XYA, AYX, YAX, YXA }
+# E = { AXY, XYA }
+#
+# ------------------- 1.2.5
+#
+# Q Q N N N D P P P P
+#
+# S = { QQ, NN, PP, QN, QD, QP, ND, NP, DP }
+#
+# Drawing 26 cents = { QP }
+# 
+# Drawing > 9 but < 25 cents = { NN, DN, PD }
+#
+# Drawing 29 cents = ∅ 
+# 
+# ------------------- 1.2.7
+#
+# E U F U G = G means that E and F are subsets of G
+#  
+# If E or F occurs, then G occurs
+#
+# EFG = G means that E = F = G
+#
+# If G occurs, then E and F occur
+#
+# ------------------- 1.2.9
+#
+# EG U F
+# 
+# (F U (EE'))(E U F')
+# (F U ∅)(E U F')
+# F (E U F')
+# (FE) U (FF')
+# FE U ∅
+# FE
